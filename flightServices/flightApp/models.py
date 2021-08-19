e6d6d856c4d0098ca071091ad83a32358e11c135
Rupam Hari <hrupam55@gmail.com>
2021-08-19 18:52:19 +0530

    added null=True for middlename

--- flightServices/flightApp/models.py
+++ flightServices/flightApp/models.py
@@ -17,7 +17,7 @@ class Flight(models.Model):
 
 class Passenger(models.Model):
     firstName = models.CharField(max_length=20)
-    middleName = models.CharField(max_length=20, blank=True)
+    middleName = models.CharField(max_length=20, blank=True, null=True)
     lastName = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=13)
