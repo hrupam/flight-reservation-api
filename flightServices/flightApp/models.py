a1a1517934f77f43a509d96dc55a5aaa22fb3289
Rupam Hari <hrupam55@gmail.com>
2021-08-20 17:49:34 +0530

    auto generation of auth token on user registration

--- flightServices/flightApp/models.py
+++ flightServices/flightApp/models.py
@@ -1,4 +1,8 @@
+from django.conf import settings
 from django.db import models
+from rest_framework.authtoken.models import Token
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 
@@ -29,3 +33,9 @@ class Passenger(models.Model):
 class Reservation(models.Model):
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
     passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance, created, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
